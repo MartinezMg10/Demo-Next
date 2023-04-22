1c312b67c5b650bf330849fdc6e8b0fcaebb4c1f
MartinezMg10 <garcia.miguelangel1206@gmail.com>
2023-04-22 18:44:59 -0500

    first commit

---
+++ components/layouts/MainLayout.tsx
@@ -0,0 +1,30 @@
+import Head from 'next/head';
+import { Navbar } from '../Navbar';
+
+import styles from './MainLayout.module.css';
+import { FC } from 'react';
+
+type Props = {
+  children?: React.ReactNode
+};
+
+export const MainLayout:FC<Props> = ({ children }) => {
+    return (
+        <div className={styles.container}>
+          
+          <Head>
+            <title>Home - Miguel</title>
+            <meta name="description" content="Home Page" />
+            <link rel="icon" href="/favicon.ico" />
+          </Head>
+        
+          <Navbar />
+    
+          <main className={styles.main}>
+    
+            { children }
+    
+          </main>     
+        </div>
+      )
+};
